3656d45c430ee793d1335c10d6250fd088a2d40e
Carlos Cabanero <cc@blink.sh>
2016-11-30 20:44:41 -0500

    Using absolute paths on build script

--- build-all.sh
+++ build-all.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+cd "${BASH_SOURCE%/*}"
 echo "Building Protobuf"
-cd build-protobuf
+cd build-protobuf/
 ./build-protobuf.sh
+
 cd ..
 echo "Building Mosh"
 ./build-mosh.sh
 ./create-libmoshios-framework.sh
+
